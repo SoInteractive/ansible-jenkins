13c8b60591643f1220f97ea777cd31953bffa109
root <tgrzesik@sointeractive.pl>
2017-07-12 14:26:19 +0200

    almost working

--- tests/roles/SoInteractive.java/tests/test_default.py
+++
@@ -1,7 +0,0 @@
-from testinfra.utils.ansible_runner import AnsibleRunner
-
-testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
-
-
-def test_linux(SystemInfo):
-    assert SystemInfo.type == 'linux'
