6178f7b8e4f75b5456d8b1bccf771bdccbacea0a
Paweł Krupa <pkrupa@sointeractive.pl>
2017-07-18 10:30:57 +0200

    support multiple credentials; cleanup code

--- templates/secret-text-credentials.groovy
+++ templates/secret-text-credentials.groovy
@@ -8,7 +8,9 @@ import hudson.util.Secret
 domain = Domain.global()
 store = Jenkins.instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
 
-secretText = new StringCredentialsImpl(
-        CredentialsScope.GLOBAL, "{{ jenkins_credentials_id }}", "{{ jenkins_credentials_description }}", Secret.fromString("{{ jenkins_credentials_secret }}"))
+{% for credential in jenkins_credentials %}
+{{ credential.id }} = new StringCredentialsImpl(
+    CredentialsScope.GLOBAL, "{{ credential.id }}", "{{ credential.description }}", Secret.fromString("{{ credential.secret }}"))
+store.addCredentials(domain, {{ credential.id }})
 
-store.addCredentials(domain, secretText)
+{% endfor %}
