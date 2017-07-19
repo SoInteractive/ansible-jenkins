5b479b9aba67a53e41adbb4bec029290cc461556
tgrzesik <tgrzesik@sointeractive.pl>
2017-07-19 14:29:21 +0200

    Update basic-security.groovy

--- templates/basic-security.groovy
+++ templates/basic-security.groovy
@@ -4,15 +4,15 @@ import jenkins.model.*
 
 def instance = Jenkins.getInstance()
 
-    println "--> creating (or modifying) local user 'admin'"
+println "--> creating (or modifying) local user 'admin'"
 
-    def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-    hudsonRealm.createAccount('{{ jenkins_admin_username }}', '{{ jenkins_admin_password }}')
-    instance.setSecurityRealm(hudsonRealm)
-    
-    println "--> setting up authorization model"
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount('{{ jenkins_admin_username }}', '{{ jenkins_admin_password }}')
+instance.setSecurityRealm(hudsonRealm)
 
-    def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
-    strategy.setAllowAnonymousRead(false)
-    instance.setAuthorizationStrategy(strategy)
-    instance.save()
+println "--> setting up authorization model"
+
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+strategy.setAllowAnonymousRead(false)
+instance.setAuthorizationStrategy(strategy)
+instance.save()
