2902f1a72ed131b228f1f3523fe6fc2f28d3bd33
tgrzesik <tgrzesik@sointeractive.pl>
2017-10-13 10:31:36 +0200

    Update notification.sh

--- .travis/notification.sh
+++ .travis/notification.sh
@@ -5,19 +5,18 @@ function success {
 
   if [ "$TRAVIS_PULL_REQUEST" != "false" ]
   then
-    #MESSAGE="Travis [build no. $TRAVIS_BUILD_NUMBER](travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID) has finished successfully. Pull request no. $TRAVIS_PULL_REQUEST opened by $GIT_COMMITER can be found [here](https://github.com/${TRAVIS_REPO_SLUG}/pull/${TRAVIS_PULL_REQUEST})."
-    MESSAGE="testing success string"
+    MESSAGE="Travis [build no. ${TRAVIS_BUILD_NUMBER}](travis-ci.org/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID}) has finished successfully. Pull request no. ${TRAVIS_PULL_REQUEST} opened by ${GIT_COMMITER} can be found [here](https://github.com/${TRAVIS_REPO_SLUG}/pull/${TRAVIS_PULL_REQUEST})."
   else
-    MESSAGE="Travis [build no. $TRAVIS_BUILD_NUMBER](travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID) has finished successfully. Tag [${GIT_TAG}](https://github.com/${TRAVIS_REPO_SLUG}/releases/tag/${GIT_TAG}) was pushed to master by ${GIT_COMMITER}."
+    MESSAGE="Travis [build no. ${TRAVIS_BUILD_NUMBER}](travis-ci.org/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID}) has finished successfully. Tag [${GIT_TAG}](https://github.com/${TRAVIS_REPO_SLUG}/releases/tag/${GIT_TAG}) was pushed to master by ${GIT_COMMITER}."
   fi
 }
 
 function failure {
   if [ "$TRAVIS_PULL_REQUEST" != "false" ]
   then
-    MESSAGE="Travis [build no. $TRAVIS_BUILD_NUMBER](travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID) has failed. Pull request no. $TRAVIS_PULL_REQUEST can be found [here](https://github.com/${TRAVIS_REPO_SLUG}/pull/${TRAVIS_PULL_REQUEST})."
+    MESSAGE="Travis [build no. ${TRAVIS_BUILD_NUMBER}](travis-ci.org/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID}) has failed. Pull request no. ${TRAVIS_PULL_REQUEST} opened by ${GIT_COMMITER} can be found [here](https://github.com/${TRAVIS_REPO_SLUG}/pull/${TRAVIS_PULL_REQUEST})."
   else
-    MESSAGE="Travis [build no. $TRAVIS_BUILD_NUMBER](travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID) has failed. Please check for problems on branch [master](https://github.com/${TRAVIS_REPO_SLUG}/tree/master) which was pushed by ${GIT_COMMITER} on repo [$TRAVIS_REPO_SLUG](https://github.com/$TRAVIS_REPO_SLUG)."
+    MESSAGE="Travis [build no. ${TRAVIS_BUILD_NUMBER}](travis-ci.org/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID}) has failed. Please check for problems on branch [master](https://github.com/${TRAVIS_REPO_SLUG}/tree/master) which was pushed by ${GIT_COMMITER} on repo [${TRAVIS_REPO_SLUG}](https://github.com/${TRAVIS_REPO_SLUG})."
   fi
 }
 
@@ -31,4 +30,5 @@ else
   failure
   COLOR="#FF0000"
 fi
+
 curl -X POST --data-urlencode "payload={\"username\": \"soi\", \"attachments\": [{ \"color\": \"$COLOR\", \"text\": \"$MESSAGE\" }], \"icon_url\": \"https://maxcdn.icons8.com/office/PNG/512/Programming/bot_80-512.png\"}" "${MM_WEBHOOK}"
